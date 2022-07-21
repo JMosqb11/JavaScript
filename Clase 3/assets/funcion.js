d34742e0574e420583ec124fd97f145a476f9379
jhonier mosquera <jhoniermosquera61@gmail.com>
2022-07-21 09:46:03 -0500

    version 1.70

---
+++ Clase 3/assets/funcion.js
@@ -0,0 +1,20 @@
+function saludar(){
+    console.log("Hola mi nombres es frailejon")
+}
+
+saludar();
+
+
+function sumar(){
+    let suma = 19 + 9;
+    console.log(suma)
+}
+
+sumar();
+
+function por (){ 
+    let resta= 20-20;
+    console.log(resta)
+}
+
+resta();
