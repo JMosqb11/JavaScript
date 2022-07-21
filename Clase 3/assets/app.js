8b1cbdd4d39b3d5f0f853b92ed1d05ba79335bdc
jhonier mosquera <jhoniermosquera61@gmail.com>
2022-07-21 08:16:47 -0500

    version 1.50

---
+++ Clase 3/assets/app.js
@@ -0,0 +1,12 @@
+let array=[]
+
+console.log(array)
+
+let numeros=[1,2,3,4]
+console.table(numeros)
+let animales=['zorro', 'jirafa', 'mamut', 'murcielago']
+console.table(animales)
+let pc=['lenovo', 'asus', 'huawei', 'rezer']
+console.table(pc)
+let moviles=['samsung', 'xiaomi', 'apple', 'oppo']
+console.table(moviles)
