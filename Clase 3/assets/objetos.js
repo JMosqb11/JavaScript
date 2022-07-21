8b4b1acd1d7367f1a0ff90df129210b4609af482
jhonier mosquera <jhoniermosquera61@gmail.com>
2022-07-21 09:18:05 -0500

    version 1.60

---
+++ Clase 3/assets/objetos.js
@@ -0,0 +1,10 @@
+let celphone={
+    color: 'bluenight',
+    modelo: '12s',
+    id: 123456,
+    camaras: 3,
+    salidaDeAudio: 2,
+    propietario:['samsung', 'xiaomi']
+}
+
+console.log(celphone)
