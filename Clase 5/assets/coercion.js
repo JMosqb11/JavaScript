2aa2d7180c53413818db84bd48c2084c0c452247
jhonier mosquera <jhoniermosquera61@gmail.com>
2022-07-25 09:19:29 -0500

    1.80

---
+++ Clase 5/assets/coercion.js
@@ -0,0 +1,19 @@
+let num1 = 89, num2 = '9'
+
+let resultado = num1 + num2
+
+console.log(resultado)
+console.log(typeof resultado)
+
+let resultadoEntero = num1 + Number(num2);
+
+console.log(resultadoEntero)
+
+
+let nota1 = 3.5, nota2='4.5'
+
+let suma = num1 + parseFloat(nota2)
+
+console.log(suma)
+
+console.log(typeof suma.toString())
