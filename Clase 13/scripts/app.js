a84458572cc2d8425724a17b3558ca4b784d3189
jhonier mosquera <jhoniermosquera61@gmail.com>
2022-08-11 07:27:15 -0500

    trycash

---
+++ Clase 13/scripts/app.js
@@ -0,0 +1,16 @@
+const obtenerNumero = () => {
+  let numero = prompt("Ingrese un número");
+
+  try {
+    if (numero == "") throw "Está vacio";
+    if (isNaN(numero)) throw "No es un número";
+
+    numero = Number(numero);
+
+    if (num < 5) throw "El npumero es muy pequeño";
+    if (num > 10) throw "El número es muy grande";
+  } catch (error) {
+    console.log(error);
+  }
+};
+obtenerNumero()
